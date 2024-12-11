84ae30744231fbd2120ec58a5ecb7dea973f17c3
Lubarskiy Konstantin <lubarskiy.k@gmail.com>
2024-12-11 15:04:04 +0200

    add delete Today activate redax persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,21 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// import { Provider } from "react-redux";
+import { Provider } from "react-redux";
 import App from "./App";
 import "modern-normalize";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-// import { PersistGate } from "redux-persist/integration/react";
-// import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/store";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/*<Provider store={store}>*/}
-    {/*  <PersistGate loading={null} persistor={persistor}>*/}
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-    {/*</PersistGate>*/}
-    {/*</Provider>*/}
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </StrictMode>,
 );
