ed5f8dd4731b2430876242d4e0a2281d5fe0f0e8
Lubarskiy Konstantin <lubarskiy.k@gmail.com>
2024-12-11 13:46:53 +0200

    add base route add layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,20 @@
 import "./App.css";
+import { Route, Routes } from "react-router";
+import Layout from "./components/layout/Layout.jsx";
 import HomePage from "./pages/homepage/homepage.jsx";
 
 function App() {
-  return <HomePage />;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<h1>Main Page</h1>} />
+        <Route path="/login" element={<h1>Login Page</h1>} />
+        <Route path="/registration" element={<h1>Registion Page</h1>} />
+        <Route path="/homepage" element={<HomePage />} />
+      </Route>
+      <Route path="*" element={<h1>Not Found</h1>} />
+    </Routes>
+  );
 }
 
 export default App;
