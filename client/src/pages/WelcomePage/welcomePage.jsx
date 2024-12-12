6bff0b504ae5d8a16618ea3099d9534295817c76
Yevsovych Kateryna <159715045+evsovichka@users.noreply.github.com>
2024-12-12 20:55:38 +0200

    titles

--- client/src/pages/WelcomePage/welcomePage.jsx
+++ client/src/pages/WelcomePage/welcomePage.jsx
@@ -5,13 +5,17 @@ import {
 } from "../../tempData/welcomePagedata.jsx";
 import WhyDrinkWaterList from "../../components/WelcomePage/WhyDrinkWaterList/WhyDrinkWaterList.jsx";
 import css from "./welcomePage.module.css";
+import Button from "../../components/ui/Button/Button.jsx";
 
 export default function WelcomePage() {
   return (
     <section className={css.welcomePage}>
-      <h1>Water consumption tracker</h1>
-      <h2>Record daily water intake and track</h2>
+      <div className={css.titleBox}>
+        <h1 className={css.title}>Water consumption tracker</h1>
+        <p className={css.subTitle}>Record daily water intake and track</p>
+      </div>
       <TrackerBenefitsList data={trackerBennefits} />
+      <Button>Try tracker</Button>
       <WhyDrinkWaterList data={whyDrinkWater} />
     </section>
   );
