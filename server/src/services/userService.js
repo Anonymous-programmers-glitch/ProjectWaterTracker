d55e473fd8698168f872228ee03c48170f2df433
Irina Babenko <babenko8irina@gmail.com>
2024-12-12 21:49:06 +0200

    deleting a commit in userService

--- server/src/services/userService.js
+++ server/src/services/userService.js
@@ -9,28 +9,6 @@ export const getUserById = async (userId) => {
   return user;
 };
 
-/////////////////////
-
 export const updateUserInfo = async (userId, updates) => {
   return await UserCollection.findByIdAndUpdate(userId, updates, { new: true });
-
-  //   const updatedUser = await UserCollection.findByIdAndUpdate(userId, updates, {
-  //     new: true,
-  //   });
-  //   if (!updatedUser) {
-  //     throw createHttpError(404, 'User not found');
-  //   }
-  //   return updatedUser;
-};
-
-export const updateAvatar = async (userId, avatarUrl) => {
-  //   const updatedUser = await UserCollection.findByIdAndUpdate(
-  //     userId,
-  //     { avatarUrl },
-  //     { new: true },
-  //   );
-  //   if (!updatedUser) {
-  //     throw createHttpError(404, 'User not found');
-  //   }
-  //   return updatedUser;
 };
