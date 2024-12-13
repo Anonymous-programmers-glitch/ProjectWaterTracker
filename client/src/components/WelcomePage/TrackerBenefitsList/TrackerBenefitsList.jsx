21c09d78a48e8e8f39a80336bf45c89513778b00
Yevsovych Kateryna <159715045+evsovichka@users.noreply.github.com>
2024-12-13 17:57:38 +0200

    світла тема

--- client/src/components/WelcomePage/TrackerBenefitsList/TrackerBenefitsList.jsx
+++ client/src/components/WelcomePage/TrackerBenefitsList/TrackerBenefitsList.jsx
@@ -1,5 +1,10 @@
+import { resizeWindow } from "../../../utils/resizeWindow";
 import css from "./TrackerBenefitsList.module.css";
+import React from "react";
 export default function TrackerBenefitsList({ data }) {
+  const sizeWindow = resizeWindow();
+  const isMobile = sizeWindow < 768;
+  const size = isMobile ? "32" : "40";
   return (
     <>
       <h3 className={css.titleList}>Tracker Benefits</h3>
@@ -8,7 +13,9 @@ export default function TrackerBenefitsList({ data }) {
           return (
             <li key={item.id}>
               <div className={css.item}>
-                <div className={css.icon}>{item.icon}</div>
+                <div className={css.icon}>
+                  {React.cloneElement(item.icon, { size: size })}
+                </div>
                 {item.text}
               </div>
             </li>
--- client/src/pages/WelcomePage/welcomePage.jsx
+++ client/src/pages/WelcomePage/welcomePage.jsx
@@ -10,12 +10,12 @@ import Button from "../../components/ui/Button/Button.jsx";
 export default function WelcomePage() {
   return (
     <section className={css.welcomePage}>
-      <div className={css.titleBox}>
-        <h1 className={css.title}>Water consumption tracker</h1>
-        <p className={css.subTitle}>Record daily water intake and track</p>
-      </div>
-      <TrackerBenefitsList data={trackerBennefits} />
-      <div className={css.button}>
+      <div>
+        <div className={css.titleBox}>
+          <h1 className={css.title}>Water consumption tracker</h1>
+          <p className={css.subTitle}>Record daily water intake and track</p>
+        </div>
+        <TrackerBenefitsList data={trackerBennefits} />
         <Button cssStyle="TryTracker">Try tracker</Button>
       </div>
       <WhyDrinkWaterList data={whyDrinkWater} />
