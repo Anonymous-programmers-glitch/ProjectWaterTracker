06fa6e9774b0920897aa0c6f7b059a38edb0f4d9
Nadija Smerechuk <nadija1smerechuk@gmail.com>
2024-12-14 11:22:17 +0200

    add privatRoute

--- client/src/components/NotFoundPage/NotFoundPage.jsx
+++ client/src/components/NotFoundPage/NotFoundPage.jsx
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
 import css from "./NotFoundPage.module.css";
+import Bottle from "../HomePage/bottle";
+
 
 const NotFoundPage = () => {
   return (
     <div className={css.container}>
+      <div className={css.background}></div>
+      
       <h1>404 - Page Not Found</h1>
       <p>The page you are looking for does not exist.</p>
-      <Link to="/">Go to Home Page</Link>
+      <Link className={css.link}  to="/">Go to Home Page</Link>
     </div>
   );
 };
