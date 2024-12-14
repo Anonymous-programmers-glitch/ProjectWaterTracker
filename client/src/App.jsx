b331245924c30e4f6e8987395c4c73bcf5f32ec8
Denys Bystryi <bystryydenis211999@gmail.com>
2024-12-14 19:47:06 +0200

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,23 +3,19 @@ import { Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage/HomePage.jsx";
 import Layout from "./components/layout/Layout.jsx";
 import WelcomePage from "./pages/WelcomePage/welcomePage.jsx";
-import Settings from "./components/Settings/Settings.jsx";
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<WelcomePage />} />
-          <Route path="/welcome" element={<WelcomePage />} />
-          <Route path="/login" element={<h1>Login Page</h1>} />
-          <Route path="/registration" element={<h1>Registion Page</h1>} />
-          <Route path="/homepage" element={<HomePage />} />
-        </Route>
-        <Route path="*" element={<h1>Not Found</h1>} />
-      </Routes>
-      <Settings />
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<WelcomePage />} />
+        <Route path="/welcome" element={<WelcomePage />} />
+        <Route path="/login" element={<h1>Login Page</h1>} />
+        <Route path="/registration" element={<h1>Registion Page</h1>} />
+        <Route path="/homepage" element={<HomePage />} />
+      </Route>
+      <Route path="*" element={<h1>Not Found</h1>} />
+    </Routes>
   );
 }
 
