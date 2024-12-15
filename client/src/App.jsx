15ac372156a2f29cbd97d6cab429b2ecba535135
Nadija Smerechuk <nadija1smerechuk@gmail.com>
2024-12-15 21:46:39 +0200

    fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,25 @@
 import "./App.css";
-import { lazy } from "react";
+import { lazy, Suspense, useEffect } from "react";
 import { Route, Routes } from "react-router";
-import HomePage from "./pages/HomePage/HomePage.jsx";
+// import HomePage from "./pages/HomePage/HomePage.jsx";
 import Layout from "./components/layout/Layout.jsx";
-import WelcomePage from "./pages/WelcomePage/welcomePage.jsx";
+// import WelcomePage from "./pages/WelcomePage/welcomePage.jsx";
 import NotFoundPage from "./components/NotFoundPage/NotFoundPage.jsx";
 // import PrivateRoute from "./PrivateRoute.jsx";
 // import RestrictedRoute from "./RestrictedRoute.jsx";
 
 
-// const HomePage = lazy(() => import("./pages/HomePage/HomePage.jsx"));
-// const WelcomePage = lazy(() => import("./pages/WelcomePage/welcomePage.jsx"));
+const HomePage = lazy(() => import("./pages/HomePage/HomePage.jsx"));
+const WelcomePage = lazy(() => import("./pages/WelcomePage/welcomePage.jsx"));
 // const LoginPage = lazy(() => import("./pages/LoginPage/LoginPage"));
 // const RegisterPage = lazy(() =>import("./pages/RegisterPage/RegisterPage"));
 
 function App() {
 
 
+
   return (
+  <Suspense fallback={null}>
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<WelcomePage />} />
@@ -34,8 +36,9 @@ function App() {
         <Route path="/homepage" element={<HomePage />} />
       </Route>
       
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Suspense>
   );
 }
 
