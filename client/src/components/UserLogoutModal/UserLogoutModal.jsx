29b4ef8e931110aa7330e999a1d30aa8a34d4320
Valeriy <157248564+ValeraVB@users.noreply.github.com>
2024-12-16 18:24:04 +0200

    add TodayListModal

--- client/src/components/UserLogoutModal/UserLogoutModal.jsx
+++ client/src/components/UserLogoutModal/UserLogoutModal.jsx
@@ -1,83 +1,77 @@
 import { useEffect, useCallback } from "react";
-// import PropTypes from "prop-types";
-import { useDispatch, useSelector } from "react-redux";
+import PropTypes from "prop-types";
+import { useDispatch } from "react-redux";
+import { clearUserData } from "../../redux/userSlice";
+import axios from "axios";
 import ModalBackdrop from "../ModalBackdrop/ModalBackdrop";
-import "./UserLogoutModal.css";
-import {
-  closeLogoutModal,
-  selectLogoutModal,
-} from "../../redux/modal/modalSlice";
-import { logout } from "../../redux/auth/operations";
+import css from "./UserLogoutModal.module.css";
+import XMarkOutline from "../ui/icons/xMarkOutline";
+import Button from "../ui/Button/Button";
 
-const UserLogoutModal = () => {
+const UserLogoutModal = ({ isOpen, onClose }) => {
   const dispatch = useDispatch();
-  const isLogoutModalOpen = useSelector(selectLogoutModal);
 
-  const handleLogout = () => {
-    dispatch(logout())
-      .unwrap()
-      .then(() => dispatch(closeLogoutModal()))
-      .catch((error) => {
-        console.error("Error exiting:", error);
-      });
+  const handleLogout = async () => {
+    try {
+      await axios.post("/api/logout");
+      dispatch(clearUserData());
+      onClose();
+    } catch (error) {
+      console.error("Error on exit:", error);
+    }
   };
 
   const handleKeyDown = useCallback(
     (e) => {
       if (e.key === "Escape") {
-        dispatch(closeLogoutModal());
+        onClose();
       }
     },
-    [dispatch]
+    [onClose]
   );
 
   useEffect(() => {
-    if (isLogoutModalOpen) {
+    if (isOpen) {
       window.addEventListener("keydown", handleKeyDown);
       return () => {
         window.removeEventListener("keydown", handleKeyDown);
       };
     }
-  }, [isLogoutModalOpen, handleKeyDown]);
+  }, [isOpen, handleKeyDown]);
 
-  if (!isLogoutModalOpen) return null;
+  if (!isOpen) return null;
 
   return (
-    <ModalBackdrop onClick={() => dispatch(closeLogoutModal())}>
-      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-        <div className="modal-header-container">
-          <h2 className="modal-header">Log out</h2>
+    <ModalBackdrop onClick={onClose}>
+      <div className={css.modalContent} onClick={(e) => e.stopPropagation()}>
+        <div className={css.modalHeaderContainer}>
+          <h2 className={css.modalHeader}>Log out</h2>
           <button
-            className="modal-close"
-            onClick={() => dispatch(closeLogoutModal())}
+            className={css.modalClose}
+            onClick={onClose}
             aria-label="Close"
           >
-            <svg viewBox="0 0 24 24">
-              <line x1="4" y1="4" x2="20" y2="20" />
-              <line x1="4" y1="20" x2="20" y2="4" />
-            </svg>
+            <XMarkOutline className={css.modalCloseIcon} />
           </button>
         </div>
-        <p className="modal-text">Do you really want to leave?</p>
-        <div className="modal-buttons">
-          <button
-            onClick={() => dispatch(closeLogoutModal())}
-            className="btn-cancel"
-          >
+        <p className={css.modalText}>Do you really want to leave?</p>
+        <div className={css.modalButtons}>
+          <Button onClick={onClose} className={css.btnCancel}>
             Cancel
-          </button>
-          <button onClick={handleLogout} className="btn-logout">
+          </Button>
+          <Button onClick={handleLogout} className={css.btnLogout}>
             Log out
-          </button>
+          </Button>
         </div>
       </div>
     </ModalBackdrop>
   );
 };
 
-// UserLogoutModal.propTypes = {
-//   isOpen: PropTypes.bool.isRequired,
-//   onClose: PropTypes.func.isRequired,
-// };
+UserLogoutModal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+};
 
 export default UserLogoutModal;
+
