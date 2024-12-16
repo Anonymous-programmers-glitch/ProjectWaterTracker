eaa74f801969eca9c8b98c2f60bb9a80f20415f0
Lubarskiy Konstantin <lubarskiy.k@gmail.com>
2024-12-16 19:30:31 +0200

    fix

--- client/src/components/HomePage/myDayliCard/MyDayliCard.jsx
+++ client/src/components/HomePage/myDayliCard/MyDayliCard.jsx
@@ -3,13 +3,12 @@ import { selectUser } from "../../../redux/auth/authSlice.js";
 import css from "./mydailycard.module.css";
 
 function MyDailyCard() {
-  const dayliNorma = useSelector(selectUser);
-  console.log(dayliNorma);
+  const { dailyNorma } = useSelector(selectUser);
   return (
     <div className={css.card}>
       <p className={css.title}>My daily norma</p>
       <div className={css.cardfooter}>
-        <p className={css.text}>1.5 L</p>
+        <p className={css.text}>{dailyNorma / 1000} L</p>
         <button className={css.button}>Edit</button>
       </div>
     </div>
