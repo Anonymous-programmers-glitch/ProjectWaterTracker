bb52eb797a1403ce1b5c488459524b79e6b8a1ff
Lubarskiy Konstantin <lubarskiy.k@gmail.com>
2024-12-16 19:26:38 +0200

    +

--- client/src/components/HomePage/myDayliCard/MyDayliCard.jsx
+++ client/src/components/HomePage/myDayliCard/MyDayliCard.jsx
@@ -1,6 +1,10 @@
+import { useSelector } from "react-redux";
+import { selectUser } from "../../../redux/auth/authSlice.js";
 import css from "./mydailycard.module.css";
 
 function MyDailyCard() {
+  const dayliNorma = useSelector(selectUser);
+  console.log(dayliNorma);
   return (
     <div className={css.card}>
       <p className={css.title}>My daily norma</p>
