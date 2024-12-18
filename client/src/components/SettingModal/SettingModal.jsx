7029eaffbb966013308fa2a7aec12729a7f542bc
Denys Bystryi <bystryydenis211999@gmail.com>
2024-12-18 18:32:44 +0200

    added operations to userPhoto and added preview photo

--- client/src/components/SettingModal/SettingModal.jsx
+++ client/src/components/SettingModal/SettingModal.jsx
@@ -1,9 +1,7 @@
 import { Form, Formik, ErrorMessage, Field } from "formik";
-// import { Field } from "formik";
 import * as Yup from "yup";
 import css from "./SettingModal.module.css";
 import { useDispatch } from "react-redux";
-// import { HiArrowDownTray } from "react-icons/hi2";
 import user from "../../testUser.json";
 import Button from "../ui/Button/Button.jsx";
 import Inputs from "../ui/Inputs/Inputs.jsx";
@@ -11,13 +9,15 @@ import ModalBackdrop from "../ModalBackdrop/ModalBackdrop.jsx";
 import { useState, useEffect, useCallback } from "react";
 import { selectUser } from "../../redux/settings/selectors.js";
 import { useSelector } from "react-redux";
-import { updateUser } from "../../redux/settings/operations.js";
 import MarkOutline from "../ui/icons/xMarkOutline.jsx";
 import EyeOutline from "../../components/ui/icons/EyeOutline.jsx";
 import EyeSlashOutline from "../ui/icons/EyeSlashOutline.jsx";
 import ArrowUpTrayOutline from "../ui/icons/ArrowUpTrayOutline.jsx";
 import { closeSettingModal } from "../../redux/modal/slice.js";
 import { selectSettingModal } from "../../redux/modal/selectors.js";
+import { update } from "../../redux/user/operations.js";
+import { uploadAvatar } from "../../redux/settings/operations.js";
+// import isLoading from "../../redux/user/selectors.js";
 
 const FeedbackSchema = Yup.object().shape({
   name: Yup.string().min(1, "Name is Too Short."),
@@ -34,6 +34,9 @@ const FeedbackSchema = Yup.object().shape({
 });
 
 export default function SettingModal() {
+  const [selectedFile, setSelectedFile] = useState(null);
+  // const [preview, setPreview] = useState(user?.avatarUrl || null);
+  //
   const value = useSelector(selectUser);
   const [openPassword, setOpenPassword] = useState(false);
   const isSettingsOpen = useSelector(selectSettingModal);
@@ -68,8 +71,8 @@ export default function SettingModal() {
       idUser: value.id,
     };
     console.log(userData);
-
-    dispatch(updateUser(userData));
+    update;
+    dispatch(update(userData));
 
     actions.resetForm();
   };
@@ -85,20 +88,42 @@ export default function SettingModal() {
 
   if (!isSettingsOpen) return null;
 
+  // додавання фото
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setSelectedFile(file);
+    }
+  };
+
+  const handleButtonClick = () => {
+    if (selectedFile) {
+      const formData = new FormData();
+      formData.append("avatar", selectedFile);
+      dispatch(uploadAvatar(formData));
+      setSelectedFile(null);
+    } else {
+      document.getElementById("avatarInput").click();
+    }
+  };
+  //
+
   return (
     <ModalBackdrop onClick={() => dispatch(closeSettingModal())}>
       <div className={css.container} onClick={(e) => e.stopPropagation()}>
         <h3 className={css.photoTitle}>Your photo</h3>
         <div className={css.imgWrapper}>
           <img
-            src={initialValues.avatarUrl || user.avatar}
+            // src={initialValues.avatarUrl || user.avatar}
+            src={selectedFile ? URL.createObjectURL(selectedFile) : user.avatar}
             alt="User photo"
             className={css.photo}
           />
           <button
             type="button"
             className={css.buttonUpload}
-            onClick={() => document.getElementById("avatarInput").click()}
+            onClick={handleButtonClick}
+            // onClick={() => document.getElementById("avatarInput").click()}
           >
             {/* <HiArrowDownTray style={{ color: "407BFF" }} /> */}
             <div className={css.uploadSvg}>
@@ -109,6 +134,9 @@ export default function SettingModal() {
               id="avatarInput"
               type="file"
               name="avatar"
+              //
+              onChange={handleFileChange}
+              //
               className={css.uploadPhotoInput}
             />
           </button>
--- client/src/redux/settings/operations.js
+++ client/src/redux/settings/operations.js
@@ -1,19 +1,35 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-export const updateUser = createAsyncThunk(
-  "users/updateContact",
-  async (idUser, thunkAPI) => {
+// export const updateUser = createAsyncThunk(
+//   "users/updateContact",
+//   async (idUser, thunkAPI) => {
+//     try {
+//       await axios.patch(`/users/${idUser}`);
+//     } catch (error) {
+//       return thunkAPI.rejectWithValue(error.massage);
+//     }
+//   },
+//   {
+//     condition: (_, thunkApi) => {
+//       const reduxState = thunkApi.getState();
+//       return reduxState.auth.token !== null;
+//     },
+//   }
+// );
+
+export const updateUserPhoto = createAsyncThunk(
+  "user/updatePhoto",
+  async (formData, thunkAPI) => {
     try {
-      await axios.patch(`/users/${idUser}`);
+      const response = await axios.patch("/users/uploadPhoto", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      return response.data;
     } catch (error) {
-      return thunkAPI.rejectWithValue(error.massage);
+      return thunkAPI.rejectWithValue(error.message);
     }
-  },
-  {
-    condition: (_, thunkApi) => {
-      const reduxState = thunkApi.getState();
-      return reduxState.auth.token !== null;
-    },
   }
 );
