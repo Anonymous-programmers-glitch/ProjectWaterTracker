3fa03fbf1078ab711c4c974bbf47f32e4317c80e
Nataliia Horokhovatska <goitprojectteam10@gmail.com>
2024-12-18 07:35:40 +0200

    update button + redux

--- client/src/components/ThemeSwitcher/ThemeSwitcher.jsx
+++ client/src/components/ThemeSwitcher/ThemeSwitcher.jsx
@@ -2,8 +2,8 @@ import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { toggleTheme } from "../../redux/themeSlice/themeSlice";
 import TextButton from "../ui/TextButton/TextButton";
-import OnMoon from "../ui/icons/onMoon";
-import OnSun from "../ui/icons/onSun";
+import ToggleOff from "../ui/icons/toggleOff";
+import ToggleON from "../ui/icons/toggleOn";
 
 const ThemeSwitcher = () => {
   const theme = useSelector((state) => state.theme.theme || "light");
@@ -22,7 +22,7 @@ const ThemeSwitcher = () => {
   return (
     <TextButton
       onClick={handleToggle}
-      children={theme === "light" ? <OnMoon /> : <OnSun />}
+      children={theme === "light" ? <ToggleOff /> : <ToggleON />}
     />
   );
 };
