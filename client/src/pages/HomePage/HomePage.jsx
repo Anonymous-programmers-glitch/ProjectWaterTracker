2bb7a441a32a94aa1e495430812723e2758d7cfa
Lubarskiy Konstantin <lubarskiy.k@gmail.com>
2024-12-19 18:17:48 +0200

    refactor

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -51,29 +51,6 @@ function HomePage() {
     dispatch(fetchWaterMonth(date));
   }, [dispatch, monthState, waterRecords.length]);
 
-  function reorderData(dataMonth, currentMonth) {
-    const newData = [];
-    const countDayofMonth = dayjs(currentMonth).daysInMonth();
-    const currentDay = dayjs(currentMonth).format("D-MM-YYYY").split("-");
-    for (let i = 1; i <= countDayofMonth; i++) {
-      currentDay[0] = i;
-      const isDay = dataMonth.find(
-        (data) => data.date === currentDay.join("-"),
-      );
-
-      if (isDay) {
-        newData.push(isDay);
-      } else {
-        const defaultData = { id: "", date: "", percent: "0" };
-        defaultData.id = Math.random().toString(36);
-        defaultData.date = currentDay.join("-");
-        newData.push(defaultData);
-      }
-    }
-
-    return newData;
-  }
-
   function handleAdd() {
     dispatch(openAddModal());
   }
