1ae4affa6366049bb3187840e574cfa9ad4ab80b
StIgorek <elitelifegroup@gmail.com>
2024-12-21 02:00:52 +0200

    add notific, fix eye & psw

--- client/src/components/signin/errorMsgFn.jsx
+++
@@ -1,8 +0,0 @@
-import toast from "react-hot-toast";
-import { signup } from "../../redux/user/operations";
-
-const EmailRejacted = () => {
-  if (signup.user.email) return toast.error("This didn't work.");
-};
-
-export { EmailRejacted };
