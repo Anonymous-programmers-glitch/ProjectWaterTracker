d441eae4689d159a3f1615e57dcb726aa60b772c
Irina Babenko <babenko8irina@gmail.com>
2024-12-21 19:20:08 +0200

    change valid/auth loginUserSchema

--- server/src/validation/auth.js
+++ server/src/validation/auth.js
@@ -23,24 +23,8 @@ export const signupUserSchema = Joi.object({
 });
 
 export const loginUserSchema = Joi.object({
-  email: Joi.string().email().pattern(emailRegexp).required().messages({
-    'string.base': 'Email must be a string.',
-    'string.empty': 'Email must not be empty.',
-    'string.pattern.base': "Email must be in the format 'example@example.com'.",
-  }),
-  password: Joi.string()
-    .min(8)
-    .max(64)
-    .required()
-    .pattern(/^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/)
-    .messages({
-      'string.base': 'Password must be a string.',
-      'string.empty': 'Password must not be empty.',
-      'string.min': 'Password must be at least 8 characters long.',
-      'string.max': 'Password must be at most 64 characters long.',
-      'string.pattern.base':
-        'Password must contain at least one uppercase letter, one number, and one special character (!@#$%^&*).',
-    }),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(8).max(64).required(),
 });
 
 export const requestResetEmailSchema = Joi.object({
