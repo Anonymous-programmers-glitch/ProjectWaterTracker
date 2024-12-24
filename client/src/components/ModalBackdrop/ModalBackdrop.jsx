0efef8b52c4ad994e5bcab59a905297697a95334
Lubarskiy Konstantin <lubarskiy.k@gmail.com>
2024-12-24 20:29:34 +0200

    fix body  fix addWater

--- client/src/components/ModalBackdrop/ModalBackdrop.jsx
+++ client/src/components/ModalBackdrop/ModalBackdrop.jsx
@@ -1,17 +1,15 @@
-import PropTypes from "prop-types";
+import { useSelector } from "react-redux";
+import { selectBodyBlock } from "../../redux/modalToggle/selectors.js";
 import css from "./ModalBackdrop.module.css";
 
 const ModalBackdrop = ({ onClick, children }) => {
+
+
   return (
     <div className={css.modalbackdrop} onClick={onClick}>
       {children}
     </div>
   );
-};
-
-ModalBackdrop.propTypes = {
-  onClick: PropTypes.func.isRequired, // клик по фону
-  children: PropTypes.node.isRequired, // дочірні елементи (контент модального вікна)
 };
 
 export default ModalBackdrop;
